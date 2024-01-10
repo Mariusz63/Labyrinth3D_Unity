790084f46026d8d3a86ba2449e8188563b0f3377
Mariusz63 <ost.mariusz63@gmail.com>
2024-01-10 22:00:09 +0100

    Add dialogue system

---
+++ Labyrinth/Assets/Scripts/CharacterScripts/DialogueManager.cs
@@ -0,0 +1,159 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Timers;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] public GameObject dialogueParent;
+    [SerializeField] public TextMeshProUGUI dialogueText;
+    [SerializeField] public Button option1Button;
+    [SerializeField] public Button option2Button;
+
+    [SerializeField] public float typingSpeed = 0.05f;
+    [SerializeField] public float turnSpeed = 2f;
+
+    public List<DialogueString> dialogueList;
+
+    [Header("Player")]
+    [SerializeField] private FirstPersonController firstPersonController; // trzeba pobrac gracza aby nie mogl ruszac myszka
+    private Transform playerCamera;
+
+    private int currentDialogueIndex = 0;
+
+    private void Start()
+    {
+        dialogueParent.SetActive(false);
+        playerCamera = Camera.main.transform;
+    }
+
+    public void DialogueStart(List<DialogueString> dialogueStrings, Transform npcTransform)
+    {
+        dialogueParent.SetActive(true);
+        firstPersonController.enabled = true;
+
+        // w razie czego zmienic 
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        StartCoroutine(TurnCameraTowardsNPC(npcTransform));
+
+        dialogueList = dialogueStrings;
+        currentDialogueIndex = 0;
+
+        DisableButtons();
+
+        StartCoroutine(PrintDialogue());
+    }
+
+    private void DisableButtons()
+    {
+        option1Button.interactable = false;
+        option2Button.interactable = false;
+
+        option1Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+        option2Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+    }
+
+    private IEnumerator TurnCameraTowardsNPC(Transform NPC)
+    {
+        Quaternion startRotation = playerCamera.rotation;
+        Quaternion targetRotation = Quaternion.LookRotation(NPC.position - playerCamera.position);
+
+        float elapsedTime = 0;
+        while (elapsedTime < 1f)
+        {
+            playerCamera.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime);
+            elapsedTime += Time.deltaTime * turnSpeed;
+            yield return null;
+        }
+
+        playerCamera.rotation = targetRotation;
+    }
+
+    private bool optionSelected = false;
+
+    private IEnumerator PrintDialogue()
+    {
+        while (currentDialogueIndex < dialogueList.Count)
+        {
+            DialogueString line = dialogueList[currentDialogueIndex];
+
+            line.startDialogueEvent?.Invoke();
+
+            if (line.isQuestion)
+            {
+                yield return StartCoroutine(TypeText(line.text));
+
+                option1Button.interactable = true;
+                option2Button.interactable = true;
+
+                option1Button.GetComponentInChildren<TMP_Text>().text = line.answerOption1;
+                option2Button.GetComponentInChildren<TMP_Text>().text = line.answerOption2;
+
+                option1Button.onClick.AddListener(() => HandleOptionSelected(line.option1IndexJump));
+                option2Button.onClick.AddListener(() => HandleOptionSelected(line.option2IndexJump));
+
+                yield return new WaitUntil(() => optionSelected);
+            }
+            else
+            {
+                yield return StartCoroutine(TypeText(line.text));
+            }
+
+            line.startDialogueEvent?.Invoke();
+            optionSelected = true;
+        }
+
+        DialogueStop();
+    }
+
+    private void DialogueStop()
+    {
+        StopAllCoroutines();
+        dialogueText.text = "";
+        dialogueParent.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    private IEnumerator TypeText(string text)
+    {
+        dialogueText.text = "";
+
+        foreach(char letter in text.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        if (!dialogueList[currentDialogueIndex].isQuestion)
+        {
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        }
+
+        if (dialogueList[currentDialogueIndex].isEnd)
+        {
+            DialogueStop();
+        }
+
+        currentDialogueIndex++;
+    }
+
+    private void HandleOptionSelected(int indexJump)
+    {
+        optionSelected = true;
+        DisableButtons();
+
+        currentDialogueIndex = indexJump;
+    }
+
+
+
+}
---
+++ Labyrinth/Assets/Scripts/CharacterScripts/DialogueTrigger.cs
@@ -0,0 +1,41 @@
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private List<DialogueString> dialogueStrings = new List<DialogueString>();
+    [SerializeField] private Transform npcTransform;
+
+    private bool hasSpoken = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !hasSpoken)
+        {
+            other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings, npcTransform);
+            hasSpoken = true;
+        }
+    }
+
+}
+
+[System.Serializable]
+public class DialogueString
+{
+    public string text; // Represent the text taht the npc says
+    public bool isEnd; // Represent if the line is the final for the converdsation
+
+    [Header("Branch")]
+    public bool isQuestion;
+    public string answerOption1;
+    public string answerOption2;
+    public int option1IndexJump;
+    public int option2IndexJump;
+
+    [Header("Triggered Events")]
+    public UnityEvent startDialogueEvent;
+    public UnityEvent endDialogueEvent;
+}
